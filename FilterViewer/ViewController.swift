8eedf0cc972f4af08b4e1681ffec1e8546630181
Guan-ho Kim <pikachu987@naver.com>
2017-07-26 16:31:07 +0900

    Initail Commit

---
+++ FilterViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FilterViewer
+//
+//  Created by pikachu987 on 2017. 7. 26..
+//  Copyright © 2017년 pikachu987. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-FilterViewer/Pods-FilterViewer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FilterViewer : NSObject
+@end
+@implementation PodsDummy_Pods_FilterViewer
+@end
